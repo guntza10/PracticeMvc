626d3524413ae7c8f1311801d61706fda4e68eef
guntza10 <guntza22@hotmail.com>
2019-10-19 15:00:52 +0700

    implement historycontroller

---
+++ TestMvc/TestMvc/Controllers/HistoryController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using TestMvc.Models;
+
+namespace TestMvc.Controllers
+{
+    public class HistoryController : Controller
+    {
+        public IMongoCollection<ShortenUrlModel> ShortenCollection { get; set; }
+        public HistoryController()
+        {
+            var client = new MongoClient("mongodb://admin:mana1234@ds016098.mlab.com:16098/shortenurl?retryWrites=false");
+            var database = client.GetDatabase("shortenurl");
+            ShortenCollection = database.GetCollection<ShortenUrlModel>("shorten2");
+        }
+
+        public IActionResult Index()
+        {
+            var historyResult = ShortenCollection.Find(it => true).SortByDescending(it => it.CreationDateTime).ToList();
+            return View(historyResult);
+        }
+
+        public IActionResult DeleteUrl(string id)
+        {
+            ShortenCollection.DeleteOne(it => it.Id == id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult EditModel(string id, string newFullUrl, string newCustom)
+        {
+            try
+            {
+                var fullUrl = "";
+                var shortUrl = "";
+                var custom = "";
+                var host = "https://testgun.azurewebsites.net/";
+
+                if (!String.IsNullOrEmpty(newFullUrl) && !String.IsNullOrEmpty(newCustom))
+                {
+                    if (!IsFullExist(newFullUrl) && !IsShortExist(newCustom))
+                    {
+                        fullUrl = newFullUrl;
+                        shortUrl = $"{host}{newCustom}";
+                        custom = newCustom;
+                        var defFullAndCustom = Builders<ShortenUrlModel>.Update
+                           .Set(it => it.FullUrl, fullUrl)
+                           .Set(it => it.ShortenUrl, shortUrl)
+                           .Set(it => it.Custom, custom)
+                           .Set(it => it.CreationDateTime, DateTime.Now);
+                        ShortenCollection.UpdateOne(it => it.Id == id, defFullAndCustom);
+                    }
+                }
+                else if (!String.IsNullOrEmpty(newFullUrl) && String.IsNullOrEmpty(newCustom))
+                {
+                    if (!IsFullExist(newFullUrl))
+                    {
+                        fullUrl = newFullUrl;
+                        var defFull = Builders<ShortenUrlModel>.Update
+                          .Set(it => it.FullUrl, fullUrl)
+                          .Set(it => it.CreationDateTime, DateTime.Now);
+                        ShortenCollection.UpdateOne(it => it.Id == id, defFull);
+                    }
+                }
+                else if (String.IsNullOrEmpty(newFullUrl) && !String.IsNullOrEmpty(newCustom))
+                {
+                    if (!IsShortExist(newCustom))
+                    {
+                        shortUrl = $"{host}{newCustom}";
+                        custom = newCustom;
+                        var defCustom = Builders<ShortenUrlModel>.Update
+                          .Set(it => it.ShortenUrl, shortUrl)
+                          .Set(it => it.Custom, custom)
+                          .Set(it => it.CreationDateTime, DateTime.Now);
+                        ShortenCollection.UpdateOne(it => it.Id == id, defCustom);
+                    }
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+
+        }
+
+        public bool IsFullExist(string fullUrl)
+        {
+            var qry = ShortenCollection.Find(it => it.FullUrl == fullUrl).ToList();
+            return (qry.Any()) ? true : false;
+        }
+
+        public bool IsShortExist(string shortUrl)
+        {
+            var qry = ShortenCollection.Find(it => it.ShortenUrl.Contains($"/{shortUrl}")).ToList();
+            return (qry.Any()) ? true : false;
+        }
+
+    }
+}
--- TestMvc/TestMvc/Controllers/ShortenUrlController.cs
+++ TestMvc/TestMvc/Controllers/ShortenUrlController.cs
@@ -143,85 +143,85 @@ namespace TestMvc.Controllers
             //return RedirectToAction("Index", new { shModel = data, message = model.alertMessage, color = model.colorAlert });
         }
 
-        public IActionResult DeleteUrl(string id)
-        {
-            ShortenCollection.DeleteOne(it => it.Id == id);
-            return RedirectToAction("Index");
-        }
+        //public IActionResult DeleteUrl(string id)
+        //{
+        //    ShortenCollection.DeleteOne(it => it.Id == id);
+        //    return RedirectToAction("Index");
+        //}
 
-        [HttpPost("{id}")]
-        public IActionResult EditModel(string id, string newFullUrl, string newCustom)
-        {
-            try
-            {
-                var fullUrl = "";
-                var shortUrl = "";
-                var custom = "";
-                var host = "https://testgun.azurewebsites.net/";
+        //[HttpPost("{id}")]
+        //public IActionResult EditModel(string id, string newFullUrl, string newCustom)
+        //{
+        //    try
+        //    {
+        //        var fullUrl = "";
+        //        var shortUrl = "";
+        //        var custom = "";
+        //        var host = "https://testgun.azurewebsites.net/";
 
-                if (!String.IsNullOrEmpty(newFullUrl) && !String.IsNullOrEmpty(newCustom))
-                {
-                    if (!IsFullExist(newFullUrl) && !IsShortExist(newCustom))
-                    {
-                        fullUrl = newFullUrl;
-                        shortUrl = $"{host}{newCustom}";
-                        custom = newCustom;
-                        var defFullAndCustom = Builders<ShortenUrlModel>.Update
-                           .Set(it => it.FullUrl, fullUrl)
-                           .Set(it => it.ShortenUrl, shortUrl)
-                           .Set(it => it.Custom, custom)
-                           .Set(it => it.CreationDateTime, DateTime.Now);
-                        ShortenCollection.UpdateOne(it => it.Id == id, defFullAndCustom);
-                    }
-                }
-                else if (!String.IsNullOrEmpty(newFullUrl) && String.IsNullOrEmpty(newCustom))
-                {
-                    if (!IsFullExist(newFullUrl))
-                    {
-                        fullUrl = newFullUrl;
-                        var defFull = Builders<ShortenUrlModel>.Update
-                          .Set(it => it.FullUrl, fullUrl)
-                          .Set(it => it.CreationDateTime, DateTime.Now);
-                        ShortenCollection.UpdateOne(it => it.Id == id, defFull);
-                    }
-                }
-                else if (String.IsNullOrEmpty(newFullUrl) && !String.IsNullOrEmpty(newCustom))
-                {
-                    if (!IsShortExist(newCustom))
-                    {
-                        shortUrl = $"{host}{newCustom}";
-                        custom = newCustom;
-                        var defCustom = Builders<ShortenUrlModel>.Update
-                          .Set(it => it.ShortenUrl, shortUrl)
-                          .Set(it => it.Custom, custom)
-                          .Set(it => it.CreationDateTime, DateTime.Now);
-                        ShortenCollection.UpdateOne(it => it.Id == id, defCustom);
-                    }
-                }
-                else
-                {
-                    return RedirectToAction("Index");
-                }
-                return RedirectToAction("Index");
-            }
-            catch (Exception err)
-            {
-                throw err;
-            }
+        //        if (!String.IsNullOrEmpty(newFullUrl) && !String.IsNullOrEmpty(newCustom))
+        //        {
+        //            if (!IsFullExist(newFullUrl) && !IsShortExist(newCustom))
+        //            {
+        //                fullUrl = newFullUrl;
+        //                shortUrl = $"{host}{newCustom}";
+        //                custom = newCustom;
+        //                var defFullAndCustom = Builders<ShortenUrlModel>.Update
+        //                   .Set(it => it.FullUrl, fullUrl)
+        //                   .Set(it => it.ShortenUrl, shortUrl)
+        //                   .Set(it => it.Custom, custom)
+        //                   .Set(it => it.CreationDateTime, DateTime.Now);
+        //                ShortenCollection.UpdateOne(it => it.Id == id, defFullAndCustom);
+        //            }
+        //        }
+        //        else if (!String.IsNullOrEmpty(newFullUrl) && String.IsNullOrEmpty(newCustom))
+        //        {
+        //            if (!IsFullExist(newFullUrl))
+        //            {
+        //                fullUrl = newFullUrl;
+        //                var defFull = Builders<ShortenUrlModel>.Update
+        //                  .Set(it => it.FullUrl, fullUrl)
+        //                  .Set(it => it.CreationDateTime, DateTime.Now);
+        //                ShortenCollection.UpdateOne(it => it.Id == id, defFull);
+        //            }
+        //        }
+        //        else if (String.IsNullOrEmpty(newFullUrl) && !String.IsNullOrEmpty(newCustom))
+        //        {
+        //            if (!IsShortExist(newCustom))
+        //            {
+        //                shortUrl = $"{host}{newCustom}";
+        //                custom = newCustom;
+        //                var defCustom = Builders<ShortenUrlModel>.Update
+        //                  .Set(it => it.ShortenUrl, shortUrl)
+        //                  .Set(it => it.Custom, custom)
+        //                  .Set(it => it.CreationDateTime, DateTime.Now);
+        //                ShortenCollection.UpdateOne(it => it.Id == id, defCustom);
+        //            }
+        //        }
+        //        else
+        //        {
+        //            return RedirectToAction("Index");
+        //        }
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch (Exception err)
+        //    {
+        //        throw err;
+        //    }
 
-        }
+        //}
 
-        public bool IsFullExist(string fullUrl)
-        {
-            var qry = ShortenCollection.Find(it => it.FullUrl == fullUrl).ToList();
-            return (qry.Any()) ? true : false;
-        }
+        //public bool IsFullExist(string fullUrl)
+        //{
+        //    var qry = ShortenCollection.Find(it => it.FullUrl == fullUrl).ToList();
+        //    return (qry.Any()) ? true : false;
+        //}
 
-        public bool IsShortExist(string shortUrl)
-        {
-            var qry = ShortenCollection.Find(it => it.ShortenUrl.Contains($"/{shortUrl}")).ToList();
-            return (qry.Any()) ? true : false;
-        }
+        //public bool IsShortExist(string shortUrl)
+        //{
+        //    var qry = ShortenCollection.Find(it => it.ShortenUrl.Contains($"/{shortUrl}")).ToList();
+        //    return (qry.Any()) ? true : false;
+        //}
         //public IActionResult Index(ShortenUrlModel shModel, string message, bool color)
         //{
         //    var historyResult = ShortenCollection.Find(it => true).SortByDescending(it => it.CreationDateTime).ToList();
